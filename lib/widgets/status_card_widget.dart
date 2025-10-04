d4e79a486e9d53015ed095fb2c385cf423f6a714
bsogulcan <bsogulcan@gmail.com>
2025-10-04 18:55:25 +0300

    refactors: moves subscription logic to utils
    moves subscription status calculation and display logic from widgets to a dedicated utility class.
    
    this improves code organization and promotes reusability of subscription-related functions.
    it also fixes an off-by-one error in the remaining days calculation, now including the last day of the subscription.
    
    Fixes: #27

--- lib/widgets/status_card_widget.dart
+++ lib/widgets/status_card_widget.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:another_iptv_player/models/api_response.dart';
 import 'package:another_iptv_player/l10n/localization_extension.dart';
+import 'package:another_iptv_player/utils/subscription_utils.dart';
 
 class StatusCardWidget extends StatelessWidget {
   final ApiResponse? serverInfo;
 
   const StatusCardWidget({super.key, required this.serverInfo});
-
-  String _getRemainingDays(BuildContext context) {
-    if (serverInfo?.userInfo.expDate != null) {
-      final expDate = serverInfo!.userInfo.expDate;
-      try {
-        final expiryDate = DateTime.fromMillisecondsSinceEpoch(
-          int.parse(expDate.toString()) * 1000,
-        );
-        final now = DateTime.now();
-        final difference = expiryDate.difference(now).inDays;
-        return difference > 0
-            ? context.loc.remaining_day(difference.toString())
-            : context.loc.expired;
-      } catch (e) {
-        return context.loc.not_found_in_category;
-      }
-    }
-    return context.loc.not_found_in_category;
-  }
 
   String _getServerStatus(BuildContext context) {
     return serverInfo != null
@@ -32,42 +14,25 @@ class StatusCardWidget extends StatelessWidget {
         : context.loc.no_connection;
   }
 
-  Color _getStatusColor(BuildContext context) {
-    if (serverInfo != null) {
-      final remaining = _getRemainingDays(context);
-      if (remaining == context.loc.expired) return Colors.red;
-      if (remaining.contains(context.loc.day)) {
-        final RegExp numberRegex = RegExp(r'\d+');
-        final match = numberRegex.firstMatch(remaining);
-        if (match != null) {
-          final days = int.tryParse(match.group(0)!) ?? 0;
-          if (days <= 7) return Colors.orange;
-          return Colors.green;
-        }
-      }
-    }
-    return Colors.grey;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(
-          _getStatusColor(context) == Colors.green
+          SubscriptionUtils.getStatusColor(serverInfo, context) == Colors.green
               ? Icons.check_circle
-              : _getStatusColor(context) == Colors.orange
+              : SubscriptionUtils.getStatusColor(serverInfo, context) == Colors.orange
               ? Icons.warning
               : Icons.error,
-          color: _getStatusColor(context),
+          color: SubscriptionUtils.getStatusColor(serverInfo, context),
           size: 36,
         ),
         title: Text(
           _getServerStatus(context),
-          style: TextStyle(color: _getStatusColor(context)),
+          style: TextStyle(color: SubscriptionUtils.getStatusColor(serverInfo, context)),
         ),
         subtitle: Text(
-          context.loc.subscription_remaining_day(_getRemainingDays(context)),
+          context.loc.subscription_remaining_day(SubscriptionUtils.getRemainingDays(serverInfo, context)),
         ),
       ),
     );
--- lib/widgets/subscription_info_widget.dart
+++ lib/widgets/subscription_info_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:another_iptv_player/models/api_response.dart';
 import 'package:another_iptv_player/l10n/localization_extension.dart';
+import 'package:another_iptv_player/utils/subscription_utils.dart';
 import 'info_tile_widget.dart';
 import 'section_title_widget.dart';
 
@@ -9,41 +10,6 @@ class SubscriptionInfoWidget extends StatelessWidget {
 
   const SubscriptionInfoWidget({super.key, required this.serverInfo});
 
-  String _getRemainingDays(BuildContext context) {
-    if (serverInfo?.userInfo.expDate != null) {
-      final expDate = serverInfo!.userInfo.expDate;
-      try {
-        final expiryDate = DateTime.fromMillisecondsSinceEpoch(
-          int.parse(expDate.toString()) * 1000,
-        );
-        final now = DateTime.now();
-        final difference = expiryDate.difference(now).inDays;
-        return difference > 0
-            ? context.loc.remaining_day(difference.toString())
-            : context.loc.expired;
-      } catch (e) {
-        return context.loc.not_found_in_category;
-      }
-    }
-    return context.loc.not_found_in_category;
-  }
-
-  Color _getStatusColor(BuildContext context) {
-    if (serverInfo != null) {
-      final remaining = _getRemainingDays(context);
-      if (remaining == context.loc.expired) return Colors.red;
-      if (remaining.contains(context.loc.day)) {
-        final RegExp numberRegex = RegExp(r'\d+');
-        final match = numberRegex.firstMatch(remaining);
-        if (match != null) {
-          final days = int.tryParse(match.group(0)!) ?? 0;
-          if (days <= 7) return Colors.orange;
-          return Colors.green;
-        }
-      }
-    }
-    return Colors.grey;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +23,8 @@ class SubscriptionInfoWidget extends StatelessWidget {
               InfoTileWidget(
                 icon: Icons.schedule,
                 label: context.loc.remaining_day_title,
-                value: _getRemainingDays(context),
-                valueColor: _getStatusColor(context),
+                value: SubscriptionUtils.getRemainingDays(serverInfo, context),
+                valueColor: SubscriptionUtils.getStatusColor(serverInfo, context),
               ),
               if (serverInfo?.userInfo != null)
                 InfoTileWidget(
